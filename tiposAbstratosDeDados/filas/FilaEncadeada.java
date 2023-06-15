445bf3f475222038e22828dc43c454900a1ed121
jhonatan <jhonatan.santos13@academico.ifs.edu.br>
2023-06-15 09:02:42 -0300

    fila encadeada adicionada

--- tiposAbstratosDeDados/filas/FilaEncadeada.java
+++ tiposAbstratosDeDados/filas/FilaEncadeada.java
@@ -1,4 +1,37 @@
 package tiposAbstratosDeDados.filas;
 
-public class FilaEncadeada {
+public class FilaEncadeada extends Fila{
+    private No inicio;
+
+    private No fim;
+    private No aux;
+
+    public FilaEncadeada(){
+        this.inicio = null;
+        this.fim = null;
+        this.aux = null;
+    }
+    @Override
+    void enfileirar(int dado) throws Exception {
+        No novo = new No();
+        novo.setProx(null);
+        if(this.inicio == null){
+            this.inicio = novo;
+            this.fim = this.inicio;
+        }
+        else{
+            this.fim.setProx(novo);
+            this.fim = novo;
+        }
+    }
+
+    @Override
+    void desenfileirar() throws Exception {
+        if(this.fim == null){
+            throw new Exception("fila vazia");
+        }
+        this.aux = this.inicio;
+        this.inicio = this.inicio.getProx();
+        this.aux = null;
+    }
 }
---
+++ tiposAbstratosDeDados/filas/No.java
@@ -0,0 +1,22 @@
+package tiposAbstratosDeDados.filas;
+
+public class No {
+    private No dado;
+    private No prox;
+
+    public No getDado() {
+        return dado;
+    }
+
+    public void setDado(No dado) {
+        this.dado = dado;
+    }
+
+    public No getProx() {
+        return prox;
+    }
+
+    public void setProx(No prox) {
+        this.prox = prox;
+    }
+}
